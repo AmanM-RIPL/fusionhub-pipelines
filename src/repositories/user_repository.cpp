bbb7b822a4162354c2c7f7d653d226dd2b44f99b
bhabananda-repl <Bhabananda.Sikdar@replinfosys.com>
2025-09-13 13:19:45 +0530

    Code changes done in repositories
    Code changes done in user_repositories and project_repositories

--- src/repositories/user_repository.cpp
+++ src/repositories/user_repository.cpp
@@ -1,10 +1,10 @@
 #include "repositories/user_repository.h"
 #include <QDebug>
 
-UserRepository::UserRepository()
+/*UserRepository::UserRepository()
 {
-}
-
+}*/
+UserRepository::UserRepository(QObject* parent) : QObject(parent) {}
 std::unique_ptr<User> UserRepository::findById(int id)
 {
     QSqlQuery query(dbManager->getDatabase());
@@ -23,12 +23,25 @@ std::vector<std::unique_ptr<User>> UserRepository::findAll()
     std::vector<std::unique_ptr<User>> users;
     QSqlQuery query(dbManager->getDatabase());
     
-    if (query.exec("SELECT * FROM User")) {
+    if (query.exec("SELECT user_id, user_name, user_fullname FROM User")) {
         while (query.next()) {
             users.push_back(mapFromQuery(query));
         }
     }
     
+    return users;
+}
+
+std::vector<User*> UserRepository::findAllQML() {
+    std::vector<User*> users;
+    QSqlQuery query(dbManager->getDatabase());
+
+    if (query.exec("SELECT * FROM User")) {
+        while (query.next()) {
+            users.push_back(mapFromQueryQML(query, this));
+        }
+    }
+
     return users;
 }
 
@@ -38,6 +51,14 @@ bool UserRepository::save(const User& entity)
     query.prepare(getInsertQuery());
     bindEntityToQuery(query, entity);
     
+    return query.exec();
+}
+
+bool UserRepository::saveQML(User* entity) {
+
+    QSqlQuery query(dbManager->getDatabase());
+    query.prepare(getInsertQuery());
+    bindEntityToQuery(query, *entity);
     return query.exec();
 }
 
@@ -78,6 +99,7 @@ std::vector<std::unique_ptr<User>> UserRepository::findByApprovalStatus(bool sta
 
 std::unique_ptr<User> UserRepository::findByUserId(const QString& userId)
 {
+
     QSqlQuery query(dbManager->getDatabase());
     query.prepare("SELECT * FROM User WHERE user_id = ?");
     query.addBindValue(userId);
@@ -85,7 +107,7 @@ std::unique_ptr<User> UserRepository::findByUserId(const QString& userId)
     if (query.exec() && query.next()) {
         return mapFromQuery(query);
     }
-    
+
     return nullptr;
 }
 
@@ -114,22 +136,67 @@ std::unique_ptr<User> UserRepository::mapFromQuery(const QSqlQuery& query) const
     user->setGlobalId(query.value("global_id").toString());
     user->setApprovalStatus(query.value("approval_status").toBool());
     user->setUserId(query.value("user_id").toString());
-    user->setUsername(query.value("username").toString());
+    user->setUserName(query.value("user_name").toString());
+    user->setUserFullName(query.value("user_fullname").toString());
+    user->setUserMobile1(query.value("user_mobile1").toString());
+    user->setUserMobile2(query.value("user_mobile2").toString());
+    user->setUserEmail1(query.value("user_email1").toString());
+    user->setUserEmail2(query.value("user_email2").toString());
+    user->setUserJobTitle(query.value("user_jobTitle").toString());
+    user->setUserStartDate(query.value("user_startDate").toString());
+    user->setUserEndDate(query.value("user_endDate").toString());
+    user->setUserMonthlyDeskCostValue(query.value("user_monthlyDeskCostValue").toString());
+
     
+    return user;
+}
+
+User* UserRepository::mapFromQueryQML(const QSqlQuery& query, QObject* parent) const {
+    auto user = new User(parent);
+    user->setId(query.value("id").toInt());
+    user->setGlobalId(query.value("global_id").toString());
+    user->setApprovalStatus(query.value("approval_status").toBool());
+    user->setUserId(query.value("user_id").toString());
+    user->setUserFullName(query.value("user_fullname").toString());
+    user->setUserName(query.value("user_name").toString());
+    user->setUserMobile1(query.value("user_mobile1").toString());
+    user->setUserMobile2(query.value("user_mobile2").toString());
+    user->setUserEmail1(query.value("user_email1").toString());
+    user->setUserEmail2(query.value("user_email2").toString());
+    user->setUserJobTitle(query.value("user_jobTitle").toString());
+    user->setUserStartDate(query.value("user_startDate").toString());
+    user->setUserEndDate(query.value("user_endDate").toString());
+    user->setUserMonthlyDeskCostValue(query.value("user_monthlyDeskCostValue").toString());
+
     return user;
 }
 
 void UserRepository::bindEntityToQuery(QSqlQuery& query, const User& entity) const
 {
+    query.addBindValue(entity.getId());
     query.addBindValue(entity.getGlobalId());
     query.addBindValue(entity.getApprovalStatus());
-    query.addBindValue(entity.getUserId());
-    query.addBindValue(entity.getUsername());
+    query.addBindValue(entity.getUserId());    
+    query.addBindValue(entity.getUserFullName());
+    query.addBindValue(entity.getUserName());
+    query.addBindValue(entity.getUserMobile1());
+    query.addBindValue(entity.getUserMobile2());
+    query.addBindValue(entity.getUserEmail1());
+    query.addBindValue(entity.getUserEmail2());
+    query.addBindValue(entity.getUserJobTitle());
+    query.addBindValue(entity.getUserStartDate());
+    query.addBindValue(entity.getUserEndDate());
+    query.addBindValue(entity.getUserMonthlyDeskCostValue());
 }
 
 QString UserRepository::getInsertQuery() const
 {
-    return "INSERT INTO User (global_id, approval_status, user_id, username) VALUES (?, ?, ?, ?)";
+    //return "INSERT INTO User (global_id, approval_status, user_id, username) VALUES (?, ?, ?, ?)";
+
+    return "INSERT INTO User (id, global_id, approval_status, user_id, user_fullname, user_name, "
+           "user_mobile1, user_mobile2, user_email1, user_email2, user_jobTitle, user_startDate,"
+           " user_endDate, user_monthlyDeskCostValue )"
+           " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 }
 
 QString UserRepository::getUpdateQuery() const
--- src/repositories/user_repository.h
+++ src/repositories/user_repository.h
@@ -5,11 +5,13 @@
 #include "models/user.h"
 #include <memory>
 
-class UserRepository : public AbstractRepository<User>
+class UserRepository : public QObject, public AbstractRepository<User>
 {
+    Q_OBJECT
+
 public:
-    UserRepository();
-    
+    //UserRepository();
+    explicit UserRepository(QObject* parent = nullptr);
     std::unique_ptr<User> findById(int id) override;
     std::vector<std::unique_ptr<User>> findAll() override;
     bool save(const User& entity) override;
@@ -21,12 +23,18 @@ public:
     std::unique_ptr<User> findByUserId(const QString& userId);
     std::unique_ptr<User> findByUsername(const QString& username);
 
+    Q_INVOKABLE std::vector<User*> findAllQML();
+    Q_INVOKABLE bool saveQML(User* entity);
+
 protected:
     QString getTableName() const override;
     std::unique_ptr<User> mapFromQuery(const QSqlQuery& query) const override;
+    User* mapFromQueryQML(const QSqlQuery& query, QObject* parent) const;
+
     void bindEntityToQuery(QSqlQuery& query, const User& entity) const override;
     QString getInsertQuery() const override;
     QString getUpdateQuery() const override;
+
 };
 
 #endif // USER_REPOSITORY_H
