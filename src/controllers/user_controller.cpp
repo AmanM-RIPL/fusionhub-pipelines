288554cf400d7bc88bfde6283daba206a880a1cb
bhabananda-repl <Bhabananda.Sikdar@replinfosys.com>
2025-09-13 13:07:14 +0530

    Code changes done in Controller
    Code changes done in user_controller.h, user_controller.cpp and project_controller.h, project_controller.cpp

--- src/controllers/user_controller.cpp
+++ src/controllers/user_controller.cpp
@@ -12,3 +12,49 @@ bool UserController::login(const QString &username, const QString &password) con
     qInfo() << username << " " << password;
     return true; // implement NetworkManager later
 }
+
+
+void UserController::create(const QString& user_fullname,
+                        const QString& user_name,
+                        const QString& mobile1,
+                        const QString& mobile2,
+                        const QString& email1,
+                        const QString& email2,
+                        const QString& jobTitle,
+                        const QString& startDate,
+                        const QString& endDate,
+                        const QString& monthlyDeskCostValue) const
+{
+    User user;
+
+    user.setId(123);
+    user.setGlobalId("123");
+    user.setApprovalStatus(true);
+    user.setUserId("010d");
+
+    user.setUserFullName(user_fullname);
+    user.setUserName(user_name);
+    user.setUserMobile1(mobile1);
+    user.setUserMobile2(mobile2);
+    user.setUserEmail1(email1);
+    user.setUserEmail2(email2);
+    user.setUserJobTitle(jobTitle);
+    user.setUserStartDate(startDate);
+    user.setUserEndDate(endDate);
+    user.setUserMonthlyDeskCostValue(monthlyDeskCostValue);
+
+    if(m_userRepository->saveQML(&user))
+    {
+        qDebug()<<"Data Saved";
+    }
+    else
+    {
+        qDebug()<<"Data not Saved";
+    }
+}
+
+std::vector<User*> UserController::getUserList() const
+{
+    return m_userRepository->findAllQML();
+}
+
--- src/controllers/user_controller.h
+++ src/controllers/user_controller.h
@@ -11,6 +11,21 @@ public:
     explicit UserController(QObject *parent = nullptr);
     Q_INVOKABLE bool login(const QString& username, const QString& password) const;
 
+
+    Q_INVOKABLE void create(const QString& userFullName,
+                            const QString& userName,
+                            const QString& mobile1,
+                            const QString& mobile2,
+                            const QString& email1,
+                            const QString& email2,
+                            const QString& jobTitle,
+                            const QString& startDate,
+                            const QString& endDate,
+                            const QString& monthlyDeskCostValue) const;
+
+    Q_INVOKABLE std::vector<User*> getUserList() const;
+
+
 signals:
 
 private:
